097f48fa906b2c08e32a3d721d267a3ff37e8964
Marius Bjørnstad <pmb@fa2k.net>
2022-08-20 15:39:15 +0200

    make various effects and working code

--- pylys/main.py
+++ pylys/main.py
@@ -1,3 +1,5 @@
+from code import InteractiveInterpreter
+from operator import is_
 import time
 import sys
 import struct
@@ -5,14 +7,19 @@ import socket
 from dataclasses import dataclass
 from tracemalloc import start
 import colorsys
+from turtle import speed
 import numpy as np
 from numpy.random import default_rng
+from paho.mqtt import client as mqtt
+
+
 rng = default_rng()
 
 
 lasttime = time.time()
-target_fps = 100
-
+TARGET_FPS = 100
+# Reduce fps when output doesn't change?
+STATIC_OUTPUT_SKIP_N_FRAMES = 10000
 
 
 def make_packet(data, universe):
@@ -72,28 +79,12 @@ make_segment(physical_positions, 224, [1,0.1,3], [1,4,3], 224)
 make_segment(physical_positions, 448, [2,0.1,3], [2,4,3], 224)
 make_segment(physical_positions, 672, [3,0.1,3], [3,4,3], 223)
 
-make_segment(physical_positions, 895, [2.5,0,1], [3,0,1], 3)
+make_segment(physical_positions, 895, [2,0,1], [3,0,1], 3)
 
 # Visual effects generation
 
-class Para:
-    """Time-dependent parameter interface."""
-    def get(self, t : float):
-        return 0
-
-
-class Const(Para):
-    """Parameter that does not depend on time."""
-
-    def __init__(self, data : np.array):
-        self.data = data
-
-    def get(self, t):
-        return self.data
-
-
-def make_color(r : float, g : float, b : float):
-    return Const(np.array([r,g,b]))
+def make_color_from_hsv(h : float, s : float, v : float):
+    return np.array(colorsys.hsv_to_rgb(h, s, v), dtype=np.float32)
 
 
 class Layer:
@@ -105,6 +96,49 @@ class Layer:
         pass
 
 
+class ScrollingRainbow(Layer):
+    """Scrolling color gradient along an axis.
+    It's not a gradient, but varies the hue of the colours along an axis.
+    The length of the scrolling vector determines the physical size of
+    one period - 360 degrees of hue. The scrolling_period determines the
+    number of seconds for one temporal period.
+    """
+    def __init__(self, physical_positions: np.array,
+                    scrolling_vector : np.array,
+                    scrolling_period : float,
+                    brightness : np.array
+                    ):
+        self.brightness = brightness
+        self.scrolling_period = scrolling_period
+        self.physical_phase = np.dot(physical_positions, scrolling_vector)
+    
+    def get_layer(self, t):
+        tt = t / self.scrolling_period - t // self.scrolling_period
+        hues = ((self.physical_phase + tt) * 360) % 360
+        rgb_data = np.stack([
+            120 - np.minimum(hues, 360-hues),
+            120 - np.abs(120 - hues),
+            120 - np.abs(240 - hues)
+        ], axis=1).clip(0, 60) * (self.brightness / 60)
+        return (rgb_data, self)
+
+
+class PlanarSplit(Layer):
+    def __init__(self, physical_positions : np.array,
+                intercept : np.array, normal : np.array,
+                under : Layer, over : Layer):
+        
+        shifted = physical_positions - intercept
+        # take dot product of positions and normal vector norm
+        self.mask = (np.dot(shifted, normal) > 0).reshape((len(physical_positions), 1))
+        self.under = under
+        self.over = over
+    
+    def get_layer(self, t : float):
+        (dn, self.under) = self.under.get_layer(t)
+        (up, self.over) = self.over.get_layer(t)
+        return (np.where(self.mask, up, dn), self)
+
 
 class Clouds(Layer):
     pass
@@ -113,22 +147,64 @@ class Clouds(Layer):
 class Fill(Layer):
     """Generate a specific colour."""
 
-    def __init__(self, size_in_pixels : int, color : Para):
-        self.size = size_in_pixels
-        self.color = color
+    def __init__(self, size_in_pixels : int, color):
+        self.buffer = np.ones((size_in_pixels, 3)) * color
 
     def get_layer(self, t):
-        return (np.ones((self.size, 3)) * self.color.get(t), self)
+        return (self.buffer, self)
 
 
-class CircleFader:
+class RadialFader(Layer):
+    """Fade all pixels the in a spherical pattern."""
 
-    def get(self, t):
-        return 0
+    def __init__(self, 
+                physical_positions,
+                start_time : float,
+                center_point : np.array, speed_meters_per_second : float,
+                from_layer : Layer, to_layer : Layer,
+                soft : float = 0.2,
+                direction_out : bool = True 
+                ):
+        self.from_layer = from_layer
+        self.to_layer = to_layer
+        self.transition_time = soft / speed_meters_per_second
+        distances = np.sqrt(np.sum((physical_positions - center_point) ** 2, axis=1))
+        self.time_offsets = (distances / speed_meters_per_second) + start_time
+        max_time = np.max(self.time_offsets)
+        if not direction_out:
+            self.time_offsets = max_time - self.time_offsets + start_time
+        self.end_time = max_time + self.transition_time
+    
+    def get_layer(self, t):
+        (to_data, self.to_layer) = self.to_layer.get_layer(t)
+        if t > self.end_time:
+            return (to_data, self.to_layer)
+        (from_data, self.from_layer) = self.from_layer.get_layer(t)
+        progresses = np.clip((t - self.time_offsets) / self.transition_time, 0, 1).reshape((to_data.shape[0], 1))
+        return (to_data * progresses + from_data * (1-progresses), self)
 
 
-class GradientFader:
-    pass
+class LinearGradient(Layer):
+    """Constant gradient spatial fader with no time dependence.
+    The length of fade_vector determines the sharpness of the transition.
+    If fade_vector is 2 m, then the fade spans 0.5 m."""
+
+    def __init__(self, 
+                physical_positions,
+                fade_start_point : np.array,
+                fade_vector : np.array,
+                from_layer : Layer, to_layer : Layer
+                ):
+        self.from_layer = from_layer
+        self.to_layer = to_layer
+        self.fade_amounts = np.clip(
+            np.dot((physical_positions - fade_start_point), fade_vector),
+            0, 1).reshape((len(physical_positions), 1))
+    
+    def get_layer(self, t):
+        (to_data, self.to_layer) = self.to_layer.get_layer(t)
+        (from_data, self.from_layer) = self.from_layer.get_layer(t)
+        return (to_data * self.fade_amounts + from_data * (1-self.fade_amounts), self)
 
 
 class GlobalFader(Layer):
@@ -150,8 +226,8 @@ class GlobalFader(Layer):
         progress = max((t - self.start_time) / self.interval, 0)
         if progress >= 1:
             return (to_data, self.to_layer)
-        elif self.fade_mode == 'linear':
-            return (to_data * (1-progress) + from_data * progress, self)
+        elif self.mode == 'linear':
+            return (to_data * progress + from_data * (1-progress), self)
         else:
             raise ValueError(f"Invalid fade mode {self.fade_mode}.")
 
@@ -160,17 +236,6 @@ class GlobalFader(Layer):
             return self.to_layer
         else:
             return self
-
-
-class RgbGamma:
-    """Empirical gamma curve to make RGB lights nicer."""
-
-    def __init__(self, parent : Layer):
-        self.parent = parent
-    
-    def get_layer(self, t):
-        (data, self.parent) = self.parent.get_layer(t)
-        return data ** 2.1
 
 
 class StableSpatialDithering:
@@ -187,15 +252,12 @@ class StableSpatialDithering:
     averages.
     """
     
-    def __init__(self, layer : Layer, rng=None):
-        self.layer = layer
+    def __init__(self, rng=None):
         self.last_outputs = np.array(0)
         self.last_stresses = np.array(0)
         self.rng = rng or np.random.default_rng()
 
-    def next(self, t):
-        (data, self.layer) = self.layer.get_layer(t)
-        
+    def next(self, data):
         targets = data * 255
 
         # If the outputs are now wrong by >1 unit, plus or minus, we
@@ -203,9 +265,11 @@ class StableSpatialDithering:
         # "last outputs" are modified so the following algorithm can
         # randomise the rounding, from scratch.
         is_wrong_output = np.abs(targets - self.last_outputs) > 1
-        if len(self.last_outputs.shape) > 0:    
-            self.last_outputs[is_wrong_output] = np.floor(targets)
-            self.last_stresses[is_wrong_output] = 0
+        if len(self.last_outputs.shape) > 0:
+            np.putmask(self.last_outputs, is_wrong_output, np.floor(targets))
+            np.putmask(self.last_stresses, is_wrong_output, 0)
+        else:#dbg
+            self.last_outputs = np.zeros(targets.shape)
 
         stresses = targets - self.last_outputs
         # Determine if the outputs should change when the target changes. 
@@ -219,75 +283,231 @@ class StableSpatialDithering:
         interval_remain = 1 - np.abs(self.last_stresses)
 
         increments = np.sign(stresses) * (
-            interval_remain * self.rng.random() < stress_increases
+            interval_remain * self.rng.random(targets.shape) < stress_increases
         )
 
         new_values = self.last_outputs + increments
         self.last_outputs = new_values
-        self.last_stresses = stresses
-        return new_values.flatten()
+        self.last_stresses = targets - new_values
+        return new_values.flatten().astype(np.uint8)
 
 
 
 #-----------SETUP-----------
 
-scene = Fill(num_pixels, make_color(20, 20, 20))
+# Root scene controller manages the layer tree
+# Custom undocumented code is just for me
+class SceneController:
+    def __init__(self, num_pixels, start_color_hsv=(0.8, 0.5, 0.3), scene_gamma=1):
+        self.is_on = False
+        self.mode = None
+        self.hsv = start_color_hsv
+        self.on_layer = Fill(num_pixels, make_color_from_hsv(*start_color_hsv))
+        self.off_layer = Fill(num_pixels, np.array([0, 0, 0]))
+        self.root = self.off_layer
+        self.num_pixels = num_pixels
+        self.scene_gamma = scene_gamma
+        self._apply_new_state(True, start_color_hsv, "normal")
+    
+    def set_color(self, str_payload):
+        if str_payload == "OFF":
+            new_power_state = False
+            new_color = self.hsv
+        else:
+            new_power_state = True
+            new_color = list(self.hsv)
+            if str_payload == "ON":
+                pass
+            if str_payload == "INCREASE":
+                new_color[2] = min(1.0, new_color[2] + 0.01)
+            elif str_payload == "DECREASE":
+                new_color[2] = max(0.0, new_color[2] - 0.01)
+                if self.hsv[2] == 0.0:
+                    new_power_state = False
+            elif str_payload == "ON":
+                pass
+            else:
+                parts = str_payload.split(",")
+                try:
+                    if len(parts) == 3:
+                        h2 = float(parts[0]) / 360.0
+                        s2 = float(parts[1]) / 100.0
+                    else:
+                        h2, s2 = new_color[:2]
+                    if len(parts) == 1 or len(parts) == 3:
+                        v2 = float(parts[-1]) / 100.0
+                        new_color = [max(0, min(1, x)) for x in (h2, s2, v2)]
+                except ValueError as e:
+                    print(f"Recevied invalid color {str_payload}: '{e}'.")
+        self._apply_new_state(new_power_state, new_color, self.mode)
+
+    def set_mode(self, mode):
+        self._apply_new_state(self.is_on, self.hsv, mode)
+
+    def set_power(self, power):
+        self._apply_new_state(power == "ON", self.hsv, self.mode)
+
+    def _apply_new_state(self, new_power_state, new_color, new_mode):
+        #print("Applying", new_power_state, ",", new_color, ",", new_mode)
+        if new_power_state != self.is_on or new_color != self.hsv or new_mode != self.mode:
+            if not new_power_state:
+                new_layer = self.off_layer
+            else:
+                if new_mode == "normal": #TODO clouds
+                    new_layer1 = Fill(self.num_pixels, make_color_from_hsv(*new_color))
+                    new_layer2 = ScrollingRainbow(physical_positions, np.array([0.5, 0, 0]), 10, new_color[-1])
+                    new_layer = PlanarSplit(physical_positions, np.array([0,0,2]), np.array([0,0,1]),
+                                new_layer2, new_layer1)
+                if new_mode == "night": #TODO stars
+                    new_layer1 = Fill(self.num_pixels, make_color_from_hsv(*new_color))
+                    new_layer2 = ScrollingRainbow(physical_positions, np.array([0.5, 0, 0]), 10, new_color[-1])
+                    new_layer = PlanarSplit(physical_positions, np.array([0,0,2]), np.array([0,0,1]),
+                                new_layer2, new_layer1)
+                elif new_mode == "basic":
+                    new_layer1 = Fill(self.num_pixels, make_color_from_hsv(*new_color))
+                    new_layer2 = ScrollingRainbow(physical_positions, np.array([0.5, 0, 0]), 10, new_color[-1])
+                    new_layer = PlanarSplit(physical_positions, np.array([0,0,2]), np.array([0,0,1]),
+                                new_layer2, new_layer1)
+                elif new_mode == "cinema":
+                    new_layer = LinearGradient(physical_positions, 
+                                            np.array([0, 4, 0]),
+                                            np.array([0, -1, 0]),
+                                            Fill(self.num_pixels, make_color_from_hsv(*new_color)),
+                                            self.off_layer)
+        else: # No change
+            return
+
+        if "cinema" in [new_mode, self.mode]:
+            self.root = GlobalFader(time.time(), 4, self.root, new_layer)
+        elif self.is_on != new_power_state:
+            if new_mode == "night": #start fade from bedroom
+                center = np.array([0, 2.0, 3.0])
+                speed = 4
+            else:
+                center = np.array([1.5, 2.0, 3.0])
+                speed = 8
+            self.root = RadialFader(physical_positions, time.time(), 
+                                center, speed,
+                                self.root, new_layer, soft=2,
+                                direction_out=new_power_state)
+        elif self.mode == new_mode: # just a colour change
+            self.root = GlobalFader(time.time(), 1, self.root, new_layer)
+        else: # fallback
+            self.root = GlobalFader(time.time(), 1, self.root, new_layer)
+
+        if new_power_state:
+            self.on_layer = new_layer
+        self.is_on = new_power_state
+        self.hsv = new_color
+        self.mode = new_mode
+
+    def get_data(self, t):
+        (data, self.root) = self.root.get_layer(t)
+        return data #** self.scene_gamma
+
+scene = SceneController(num_pixels, start_color_hsv=(0.5, 0.5, 0.5), scene_gamma=2.1)
+output_dithering_adapter = StableSpatialDithering()
+
+#----- MQTT Command loop --------
+MQTT_HOST = "192.168.1.8"
+MQTT_TOPIC_BASE = "stue/lys"
+FADE_TIME = 500 / 1000.0
 
 
-channel_data_generator = StableSpatialDithering(scene)
+client = mqtt.Client()
+
+def on_connect(client, _, flags, rc):
+    client.subscribe(f"{MQTT_TOPIC_BASE}/#")
+client.on_connect = on_connect
+
+def on_message(client, state, msg):
+    try:
+        str_payload = msg.payload.decode('ascii')
+    except ValueError:
+        return
+    if str_payload == "EXIT":
+        sys.exit(0)
+    topic = msg.topic[len(f"{MQTT_TOPIC_BASE}/"):]
+    if topic == "color":
+        scene.set_color(str_payload)
+    elif topic == "mode":
+        scene.set_mode(str_payload)
+    elif topic == "power":
+        scene.set_power(str_payload)
+
+client.on_message = on_message
+client.connect(MQTT_HOST, 1883, 60)
+client.loop_start()
 
 
 #---------------------------
 
-
-
-
 preview_mode = True
 
 if preview_mode:
-    import matplotlib.pyplot as plt
-    plt.ion()
-    #ig = plt.figure()
-    
+    import pygame
+    import pygame.surfarray as surfarray
+    pygame.init()
     xmax, ymax = physical_positions[:, 0].max(), physical_positions[:, 1].max()
-    xpix, ypix = 250, 250
+    xpix, ypix = 500, 500
+    screen = pygame.display.set_mode((xpix, ypix))
     
-    image = np.zeros((xpix+1, ypix+1, 3), dtype=np.uint8)
+    image = np.zeros((xpix, ypix, 3), dtype=np.uint8)
 
-    xindexes = np.array([[(0.1 + (pp[0]/xmax)*0.8) * xpix]*3 for pp in physical_positions], dtype='uint64').reshape(-1)
+    xindexes = np.array(xmax - [[(0.1 + (pp[0]/xmax)*0.8) * xpix]*3 for pp in physical_positions], dtype='uint64').reshape(-1)
     yindexes = np.array([[(0.1 + (pp[1]/ymax)*0.8) * ypix]*3 for pp in physical_positions], dtype='uint64').reshape(-1)
     cindexes = np.array([0,1,2] * len(physical_positions), dtype='uint64').reshape(-1)
 
     def show_preview(output_buffer):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
         image[xindexes, yindexes, cindexes] = output_buffer
-        plt.imshow(image)
-        plt.draw()
-        plt.pause(0.0001)
-        #plt.clf()
-        #plt.show()
+        surfarray.blit_array(screen, image)
+        pygame.display.flip()
 
-for frame in range(10):
+prev_output_buffer = np.zeros([1])
+equal_frames_count = 0
+
+
+# ----- FRAME LOOP -----
+
+#for frame in range(100):
+while True:
     t = time.time()
 
-    output_buffer = channel_data_generator.next(t)
+    output_buffer = output_dithering_adapter.next(scene.get_data(t))
+    #output_buffer = (scene.get_data(t) * 255).astype(np.uint8).flatten()
+    do_send_output = True
+    if np.array_equal(output_buffer, prev_output_buffer):
+        equal_frames_count += 1
+        if equal_frames_count < STATIC_OUTPUT_SKIP_N_FRAMES:
+            do_send_output = False
+        else:
+            equal_frames_count = 0
+    else:
+        equal_frames_count = 0
+    prev_output_buffer = output_buffer
 
-    for i, node in enumerate(output_nodes):
-        if node.frame_skipping:
-            if outputs_frame_skipping[i] == node.frame_skipping:
-                outputs_frame_skipping[i] = 0
-            else:
-                outputs_frame_skipping[i] += 1
-                continue
+    if do_send_output:
+        for i, node in enumerate(output_nodes):
+            if node.frame_skipping:
+                if outputs_frame_skipping[i] == node.frame_skipping:
+                    outputs_frame_skipping[i] = 0
+                else:
+                    outputs_frame_skipping[i] += 1
+                    continue
 
-        packet_data = make_packet(
-            output_buffer[node.buffer_index:node.buffer_index+node.data_len].tobytes(),
-            node.universe
-            )
-        #output_socket.sendto(packet_data, (node.address, node.port))
+            packet_data = make_packet(
+                output_buffer[node.buffer_index:node.buffer_index+node.data_len].tobytes(),
+                node.universe
+                )
+            if not preview_mode:
+                output_socket.sendto(packet_data, (node.address, node.port))
 
-    if preview_mode:
-        show_preview(output_buffer)
+        if preview_mode:
+            show_preview(output_buffer)
 
     dt = t - lasttime
-    time.sleep(max(0, 1 / target_fps - dt))
+    time.sleep(max(0, 1 / TARGET_FPS - dt))
     lasttime = t
+
