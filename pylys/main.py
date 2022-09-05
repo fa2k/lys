e4824b27c47ad75cd9e6e18e8c1074b1f4b229e3
Marius Bjørnstad <pmb@fa2k.net>
2022-09-05 22:23:29 +0200

    made waves, stars, etc

--- pylys/main.py
+++ pylys/main.py
@@ -4,15 +4,14 @@ import time
 import sys
 import struct
 import socket
+import math
 from dataclasses import dataclass
-from tracemalloc import start
+from collections.abc import Sequence
 import colorsys
-from turtle import speed
 import numpy as np
 from numpy.random import default_rng
 from paho.mqtt import client as mqtt
 import threading
-import opensimplex
 
 
 rng = default_rng()
@@ -57,9 +56,9 @@ output_nodes = [
     ArtNetNode("192.168.1.224", 3, 1020, 510),
     ArtNetNode("192.168.1.224", 4, 1530, 510),
     ArtNetNode("192.168.1.224", 5, 2040, 510),
-    ArtNetNode("192.168.1.224", 6, 2550, 135),
+    ArtNetNode("192.168.1.224", 6, 2550, 153),
     # Hyllelys 3 pixels
-    ArtNetNode("192.168.1.226", 0, 2685,   9, frame_skipping=2),
+    ArtNetNode("192.168.1.226", 0, 2703,   9, frame_skipping=2),
 ]
 outputs_frame_skipping = np.zeros(len(output_nodes))
 
@@ -76,12 +75,12 @@ def make_segment(positions_array, start_index, start_point, end_point, count):
     positions_array[start_index:start_index+count, :] =  startnp + \
                                                 (endnp - startnp) * np.linspace(0, 1, num=count).reshape((count, 1))
 
-make_segment(physical_positions, 0,   [0,0.1,3], [0,4,3], 224)
-make_segment(physical_positions, 224, [1,4,3], [1,0.1,3], 224)
-make_segment(physical_positions, 448, [2,0.1,3], [2,4,3], 224)
-make_segment(physical_positions, 672, [3,4,3], [3,0.1,3], 223)
+make_segment(physical_positions, 0,   [0,0.1,3], [0,4,3], 226)
+make_segment(physical_positions, 226, [1,4,3], [1,0.1,3], 225)
+make_segment(physical_positions, 451, [2,0.1,3], [2,4,3], 224)
+make_segment(physical_positions, 675, [3,4,3], [3,0.1,3], 226)
 
-make_segment(physical_positions, 895, [2,0,1], [3,0,1], 3)
+make_segment(physical_positions, 901, [2,0,1], [3,0,1], 3)
 
 #make_segment(physical_positions, 898, [2,0,0.5], [3,0,0.5], 1)
 
@@ -144,9 +143,9 @@ class PlanarSplit(Layer):
         return (np.where(self.mask, up, dn), self)
 
 
-opensimplex.seed(1337)
+# TODO opensimplex.seed(1337)
 class Clouds(Layer):
-
+    """TODO clouds doesn't work - stuck in opensimplex"""
     starttime = 0
 
     def __init__(self, physical_positions : np.array, rgb : np.array):
@@ -170,6 +169,112 @@ class Clouds(Layer):
         self.buffer[:, 0:2] = (self.whiteclouds + 1) * minrg
         return (self.buffer, self)
 
+class WildWaves(Layer):
+    """Happy accident during development, all kinds of strange things going on."""
+    def __init__(self, physical_positions : np.array):
+        target_average_color=np.array((0.5, 0.5, 1.0))
+        background_color=np.array([0.1, 0.1, 1.0])
+        wave_colors=np.array([
+            [1.0, 0.2, 0.0],
+            [0.5, 0.5, 0.5]
+        ]).transpose()
+        wave_vectors=np.array([
+            [2, 0, 0],
+            [0.5, 1.5, 0]
+        ]).transpose()
+        wave_speeds=np.array([0.5, 0.2])
+
+        self.colors = np.hstack([wave_colors, background_color.reshape(3, 1)])
+        self.wave_vectors = wave_vectors
+        coefficients = linear_model.Ridge(alpha=.5, fit_intercept=False) \
+                                    .fit(self.colors, target_average_color) \
+                                    .coef_
+        self.wave_means = coefficients
+        self.wave_amplitudes = (1-coefficients)
+        dot_products = np.matmul(physical_positions, wave_vectors)
+        phys_lengths = np.linalg.norm(physical_positions, axis=1, keepdims=True)
+        wave_vector_sq_lengths = np.sum(np.square(wave_vectors), axis=0, keepdims=True)
+        self.positional_phases = dot_products / (phys_lengths * wave_vector_sq_lengths)
+        self.wave_speeds = wave_speeds
+
+    def get_layer(self, t: float):
+        # Set to background
+        waves = np.hstack([
+            np.sin(self.positional_phases + t * self.wave_speeds),
+            np.zeros((self.positional_phases.shape[0], 1))
+            ])
+        data = waves * self.wave_amplitudes + self.wave_means
+        return (data % 1, self)
+
+
+class Waves(Layer):
+
+    def __init__(self,
+                physical_positions : np.array,
+                wave_colors : np.array,
+                wave_vectors : np.array,
+                wave_speeds : np.array):
+        """
+        Wave colors and background: each column is a rgb colour. Average rgb across wave. Should
+                     not exceed 0.5, as it's no possible to exceed 0.5 average intensity for a 
+                     sine wave. After all the waves, the final column should contain the background
+                     colour. The background can have any valid rgb from 0 to 1.
+        Wave vectors: direction of the waves. Each column contains the xyz coordinates for
+                        a vector. The length of the vectors correspond to the wavelengths.
+        Wave speeds: array of frequencies, one per wave, in arbitrary units.
+        """
+        self.colors = wave_colors
+        dot_products = np.dot(physical_positions, wave_vectors)
+        wave_vector_sq_lengths = np.sum(np.square(wave_vectors), axis=0, keepdims=True)
+        self.positional_phases = dot_products / wave_vector_sq_lengths
+        self.wave_speeds = wave_speeds
+
+    def get_layer(self, t: float):
+        # Set to backgroun
+        variable = np.hstack([
+            np.sin(2 * math.pi * (self.positional_phases + t * self.wave_speeds)),
+            np.zeros((self.positional_phases.shape[0], 1))
+            ])
+        data = np.matmul((1+variable), self.colors.transpose())
+        return (data.clip(0,1), self)
+
+
+
+
+class Stars(Layer):
+    """Generate twinkling bright lights due to atmospheric refraction.
+    
+    The average color should be quite dark in order for this to work."""
+    def __init__(self, size_in_pixels : int, color_rgb, 
+                        fraction_of_lit_stars=0.35,
+                        twinkle_interval=300,
+                        twinkle_duration=0.2):
+        rng = np.random.default_rng()
+        self.bright_pixels_indexes = np.asarray(
+                rng.random(size=(size_in_pixels,)) < fraction_of_lit_stars
+            ).nonzero()[0]
+        n_stars = self.bright_pixels_indexes.shape[0]
+        # Lognormal distribution - arbitrary choice. Its mean is exp(0.5) = 
+        star_base_brightnesses = rng.lognormal(
+                        mean=math.log(max(1 / fraction_of_lit_stars, 0.0001))-0.5,
+                        size=(n_stars)
+                    )
+        self.star_rgb_brightnesses = \
+            star_base_brightnesses.reshape((n_stars,1)) * color_rgb.reshape((1, 3))# + \
+            #(rng.normal(0, 0.2, size=(n_stars, 3)))
+
+        self.size = size_in_pixels
+        self.twinkle_intervals = np.random.power(2, n_stars) * twinkle_interval
+        self.gaus_denom = (2 * np.power(twinkle_duration/2, 2.))
+
+    def get_layer(self, t):
+        sky = np.zeros((self.size, 3))
+        modulators = 1-np.exp(
+            -np.power((t % self.twinkle_intervals) - self.twinkle_intervals/2, 2.) / self.gaus_denom
+        )
+        sky[self.bright_pixels_indexes, :] = self.star_rgb_brightnesses * modulators.reshape((-1, 1))
+        return (sky.clip(0, 1), self)
+
 
 class Fill(Layer):
     """Generate a specific colour."""
@@ -184,7 +289,7 @@ class Fill(Layer):
 class RadialFader(Layer):
     """Fade all pixels the in a spherical pattern."""
 
-    def __init__(self, 
+    def __init__(self,
                 physical_positions,
                 start_time : float,
                 center_point : np.array, speed_meters_per_second : float,
@@ -320,6 +425,7 @@ class StableSpatialDithering:
 
 
 
+
 #-----------SETUP-----------
 
 
@@ -336,7 +442,7 @@ class SceneController:
         self.num_pixels = num_pixels
         self.scene_gamma = scene_gamma
         self.lock_event = threading.Condition()
-        self._apply_new_state(True, start_color_hsv, "normal")
+        self._apply_new_state(True, start_color_hsv, sys.argv[1] if len(sys.argv) > 1 else "normal")
     
     def set_color(self, str_payload):
         if str_payload == "OFF":
@@ -389,14 +495,10 @@ class SceneController:
             if not new_power_state:
                 new_layer = self.off_layer
             else:
-                if new_mode == "normal": #TODO clouds
-                    new_layer1 = Fill(self.num_pixels, hsv_to_rgb_array(*new_color))
-                    #new_layer1 = Clouds(physical_positions, hsv_to_rgb_array(*new_color))
-                    new_layer2 = self._make_part_scene(physical_positions, new_color[-1])
-                    new_layer = PlanarSplit(physical_positions, np.array([0,0,2]), np.array([0,0,1]),
-                                new_layer2, new_layer1)
-                if new_mode == "night": #TODO stars
-                    new_layer1 = Fill(self.num_pixels, hsv_to_rgb_array(*new_color))
+                if new_mode == "wild": #TODO clouds
+                    new_layer = WildWaves(physical_positions)
+                elif new_mode == "night": #TODO stars
+                    new_layer1 = Stars(self.num_pixels, hsv_to_rgb_array(*new_color))
                     new_layer2 = self._make_part_scene(physical_positions, new_color[-1])
                     new_layer = PlanarSplit(physical_positions, np.array([0,0,2]), np.array([0,0,1]),
                                 new_layer2, new_layer1)
@@ -411,6 +513,11 @@ class SceneController:
                                             np.array([0, -1, 0]),
                                             Fill(self.num_pixels, hsv_to_rgb_array(*new_color)),
                                             self.off_layer)
+                else: #if new_mode == "normal":
+                    new_layer1 = make_heavenly_waves(physical_positions, hsv_to_rgb_array(*new_color))
+                    new_layer2 = self._make_part_scene(physical_positions, new_color[-1])
+                    new_layer = PlanarSplit(physical_positions, np.array([0,0,2]), np.array([0,0,1]),
+                                new_layer2, new_layer1)
         else: # No change
             return
         with self.lock_event:
@@ -420,12 +527,14 @@ class SceneController:
                 if new_mode == "night": #start fade from bedroom
                     center = np.array([0, 2.0, 3.0])
                     speed = 2
+                    soft = 1
                 else:
                     center = np.array([1.5, 2.0, 3.0])
                     speed = 4
+                    soft = 0.1
                 self.root = RadialFader(physical_positions, time.time(), 
                                     center, speed,
-                                    self.root, new_layer, soft=1,
+                                    self.root, new_layer, soft=soft,
                                     direction_out=new_power_state)
             elif self.mode == new_mode: # just a colour change
                 self.root = GlobalFader(time.time(), 1, self.root, new_layer)
@@ -454,6 +563,7 @@ class SceneController:
                 return time.time() - t0
             return 0
 
+
     def get_data(self, t):
         with self.lock_event:
             (data, self.root) = self.root.get_layer(t)
@@ -464,7 +574,69 @@ class SceneController:
         return data ** self.scene_gamma
 
 
-scene = SceneController(num_pixels, start_color_hsv=(0.5, 0.5, 0.5), scene_gamma=2.1)
+def make_heavenly_waves(physical_positions, color):
+    color_remain = np.array(color).reshape(3,1)
+    # Make 2 waves by taking out as much as possible of the target colour
+    # into the waves. Whatever is left has to become the background.
+    # The waves are scaled by the Waves layer such that the average 
+    # colour across the wave is equal to the given value. The wave's rgb
+    # components should not exceed 0.5 because then the max output rgb 
+    # value will be greater than 1.0.
+    
+    waves = []
+
+    # Orange wave
+    orange_factors = np.array([1, 0.5, 0])
+    orange_intensity = np.min([(1-color_remain[0:2])*orange_factors[0:2],
+                                        color_remain[0:2]*orange_factors[0:2]])
+    orange_wave = orange_intensity*orange_factors.reshape(3, 1)
+    color_remain -= orange_wave
+    #print("orange", orange_intensity)
+    waves.append(orange_wave)
+
+    # Save a little light for background, so it doesn't go completely dark
+    saving = np.minimum(color_remain, 10/255)
+    color_remain -= saving
+
+    # White wave component:
+    # * No more than the lowest rgb component
+    # * Also no more than (1-rgb) as its brightness is limited to 50% max.
+    white_intensity = np.min([1-color_remain, color_remain])
+    white_wave = white_intensity * np.ones((3,1))
+    color_remain -= white_wave
+    #print("white", white_intensity)
+    waves.append(white_wave)
+
+    # Yellow wave
+    #yellow_intensity = np.min([1-color_remain[0:2], color_remain[0:2]])
+    #yellow_wave = np.array((yellow_intensity, yellow_intensity, 0)).reshape(3, 1)
+    #color_remain -= yellow_wave
+
+
+    # Red wave
+    red_intensity = np.min([1-color_remain[0], color_remain[0]])
+    red_wave = np.array((red_intensity, 0, 0)).reshape(3, 1)
+    color_remain -= red_wave
+    #print("red", red_intensity)
+    waves.append(red_wave)
+
+    color_remain += saving
+
+    #print("color", color, "backgrd", color_remain)
+
+    # Background is the remaining colour
+    return Waves(physical_positions,
+                        wave_colors=np.hstack(waves + [color_remain]),
+                        wave_vectors=np.array([
+                            [0.9, 0.8, 0],
+                            [0.9, 0.8, 0.0],
+                            [0.8, 1, 0.0]
+                        ][:len(waves)]).transpose(),
+                        wave_speeds=np.array([0.01, 0.024, 0.02][:len(waves)])
+                )
+
+
+scene = SceneController(num_pixels, start_color_hsv=(0.5, 0.5, 1.0), scene_gamma=2.1)
 output_dithering_adapter = StableSpatialDithering()
 
 #----- MQTT Command loop --------
@@ -504,7 +676,7 @@ client.loop_start()
 
 #---------------------------
 
-preview_mode = False
+preview_mode = (len(sys.argv) > 1)
 
 if preview_mode:
     import pygame
